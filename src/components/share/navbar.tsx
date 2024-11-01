0cab0f29ffdbba1de0d1d4a6c6945f6108ca30ae
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2024-11-02 05:51:56 +0600

    update

--- src/components/share/navbar.tsx
+++ src/components/share/navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
     <div
       className={cn(
         "fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 transition-all duration-200",
-        isScrolled ? "h-20" : "h-24"
+        isScrolled ? "h-20" : "h-24",
       )}
     >
       <nav className="max-w-7xl mx-auto px-4 h-full flex items-center justify-between">
